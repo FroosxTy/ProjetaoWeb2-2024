0fbe607e3aacb0aa922a7f50c38320dd2e946daa
FroosxTy <yuri.br.100@gmail.com>
2024-02-29 19:30:21 -0300

    the first test commit

---
+++ Web2ProjectY/src/main/java/ifba/web2/Web2ProjectYApplication.java
@@ -0,0 +1,13 @@
+package ifba.web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2ProjectYApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2ProjectYApplication.class, args);
+	}
+
+}
